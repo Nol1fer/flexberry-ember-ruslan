9799bd5ca836cd9e199cf29d0b02e670337b67b8
N0lifer <gluhov.m.n@gmail.com>
2025-02-09 13:07:29 +0500

    feat: add model and each

--- app/routes/books.js
+++ app/routes/books.js
@@ -1,4 +1,14 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    return [
+      {
+        authorName: "А.С. Пушкин", bookName: "Евгений Онегин"
+      },
+      {
+        authorName: "Л.Н. Толстой", bookName: "Война и мир"
+      }
+    ];
+  }
 });
