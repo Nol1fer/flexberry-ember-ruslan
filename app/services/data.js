f371c578522f9e5543f97b6dae02fb7a185bc71a
N0lifer <gluhov.m.n@gmail.com>
2025-02-10 09:55:26 +0500

    feat: add books refresh using 2-way binding

--- app/services/data.js
+++ app/services/data.js
@@ -1,16 +1,30 @@
 import Service from '@ember/service';
 import ENV from 'flexberry-ember-ruslan/config/environment';
+import { A } from "@ember/array";
 
 export default Service.extend({
-  getBooks() {
-    return fetch(`${ENV.backendURL}/books`).then((response) => response.json());
+  init() {
+    this._super(...arguments);
+    this.set('books', A());
+  },
+
+  async getBooks() {
+    // return await fetch(`${ENV.backendURL}/books`).then((r) => r.json());
+
+    let response = await fetch(`${ENV.backendURL}/books`);
+    let books = await response.json();
+    this.get('books').clear();
+    this.get('books').pushObjects(books);
+    return this.get('books');
   },
 
   getBook(id) {
-    return fetch(`${ENV.backendURL}/books/${id}`).then((response) => response.json());
+    // return fetch(`${ENV.backendURL}/books/${id}`).then((response) => response.json());
+    return this.get('books').find((book) => book.id === parseInt(id));
   },
 
   deleteBook(book) {
+    this.get('books').removeObject(book);
     return fetch(`${ENV.backendURL}/books/${book.id}`, { method: 'DELETE' });
   }
 });
